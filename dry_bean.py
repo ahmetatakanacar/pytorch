dc102ac38e95a91b3018503c019fad2c9175af38
Ahmet Atakan Acar <acaratkan@hotmail.com>
2026-08-08 00:25:44 +0300

    add model train and eval

--- dry_bean.py
+++ dry_bean.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import torch
 from torch import nn
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
+from torchmetrics.classification import MulticlassAccuracy, MulticlassConfusionMatrix
 
 df = pd.read_excel("Dry_Bean_Dataset.xlsx")
 
@@ -20,10 +21,14 @@ y = le.fit_transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42, stratify = y)
 
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
 X_train = torch.tensor(X_train, dtype = torch.float32)
 X_test = torch.tensor(X_test, dtype = torch.float32)
-y_train = torch.tensor(y_train, dtype = torch.long).unsqueeze(1)
-y_test = torch.tensor(y_test, dtype = torch.long).unsqueeze(1)
+y_train = torch.tensor(y_train, dtype = torch.long)
+y_test = torch.tensor(y_test, dtype = torch.long)
 
 print(X_train.shape, X_test.shape)
 print(y_train.shape, y_test.shape)
@@ -43,3 +48,37 @@ class DryBean(nn.Module):
 
       def forward(self, x):
             return self.layer_stack(x)
+
+model = DryBean()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params = model.parameters(), lr = 0.002)
+accuracy = MulticlassAccuracy(num_classes = 7)
+confusion_matrix = MulticlassConfusionMatrix(num_classes = 7)
+
+epochs = 400
+
+for epoch in range(epochs):
+
+      model.train()
+
+      logits = model(X_train)
+      loss = loss_fn(logits, y_train)
+
+      pred = torch.softmax(logits, dim = 1).argmax(dim = 1)
+      acc = accuracy(pred, y_train)
+
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+      model.eval()
+      with torch.inference_mode():
+            test_logits = model(X_test)
+            test_loss = loss_fn(test_logits, y_test)
+            test_pred = torch.softmax(test_logits, dim = 1).argmax(dim = 1)
+            test_acc = accuracy(test_pred, y_test)
+
+            if epoch % 20 == 0:
+                        print(f"Epoch: {epoch}, Loss: {loss:4f}, Accuracy: {acc:4f}, Test Loss: {test_loss:4f}, Test Accuracy: {test_acc:4f}")
+
+print(confusion_matrix(test_pred, y_test))
