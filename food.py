46225d87e4562c2ceae2a1c10807f6d8d95c0bc3
ahmetatakanacar <acaratkan@hotmail.com>
2026-09-18 12:20:13 +0300

    add dataloaders and resnet model

--- food.py
+++ food.py
@@ -1,6 +1,14 @@
+import os
 import torch
 from torch import nn
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import torchvision
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+BATCH_SIZE = 32
+NUM_WORKERS = os.cpu_count() or 1
 
 data_transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -24,3 +32,35 @@ test_data = datasets.Food101(
     download=True,
     transform=data_transform
 )
+
+class_names = train_data.classes
+
+train_dataloader = DataLoader(
+    train_data,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=NUM_WORKERS,
+    pin_memory=True)
+
+test_dataloader = DataLoader(
+    test_data,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    num_workers=NUM_WORKERS,
+    pin_memory=True)
+
+def create_model(num_classes: int, device: str):
+    weights = torchvision.models.ResNet50_Weights.DEFAULT
+    model = torchvision.models.resnet50(weights=weights)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, num_classes)
+
+    return model.to(device)
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+model = create_model(num_classes=len(class_names), device=device)
